ed1797ef8342637a9cdeac116a8e988357dd9365
jaha0025 <jaha0025@algonquinlive.com>
2020-03-16 21:36:39 -0400

    first real commit

---
+++ app/src/main/java/com/example/jaha0025/nfd/DeliveryOption.java
@@ -0,0 +1,13 @@
+package com.example.jaha0025.nfd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeliveryOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_option);
+    }
+}
