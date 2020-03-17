95c5366ef4ce9ffb808c841e146378305157e3d6
jaha0025 <jaha0025@algonquinlive.com>
2020-03-16 21:30:41 -0400

    testing

---
+++ app/src/main/java/com/example/jaha0025/nfd/OrderOption.java
@@ -0,0 +1,13 @@
+package com.example.jaha0025.nfd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_option);
+    }
+}
